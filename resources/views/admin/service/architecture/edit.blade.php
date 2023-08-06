1faa65adba7eb8c7fb928acebfa0cc35cbd62e85
mostafaelneemr <mostafa.elnemr50@gmail.com>
2023-08-07 00:25:44 +0300

    service architectures

--- resources/views/admin/service/architecture/edit.blade.php
+++ resources/views/admin/service/architecture/edit.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.admin.master')
 
 @section('title')
-    Edit testimonial
+    Edit architectures
 @endsection
 
 @section('content')
     <div class="page-title">
         <div class="row">
             <div class="col-sm-6">
-                <h4 class="mb-0">edit testimonial</h4>
+                <h4 class="mb-0">edit architectures</h4>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb pt-0 pr-0 float-left float-sm-right ">
                     <li class="breadcrumb-item"><a href="{{route('dashboard')}}" class="default-color">dashboard</a></li>
-                    <li class="breadcrumb-item active">edit testimonial section</li>
+                    <li class="breadcrumb-item active">edit architectures section</li>
                 </ol>
             </div>
         </div>
@@ -26,50 +26,41 @@
         <div class="col-md-12 mb-30">
             <div class="card card-statistics h-100">
                 <div class="card-body">
-                    <form class="form" action="{{route('testimonials.update', $testimonails->id)}}" method="POST" enctype="multipart/form-data">
+                    <form class="form" action="{{route('architecture.update', $architectures->id)}}" method="POST" enctype="multipart/form-data">
                         @method('PUT')
                         @csrf
 
-                        <input type="hidden" name="id" value="{{ $testimonails->id }}">
-                        <input type="hidden" name="old_image" value="{{ $testimonails->image }}">
+                        <input type="hidden" name="id" value="{{ $architectures->id }}">
+                        <input type="hidden" name="old_image" value="{{ $architectures->icon }}">
 
                         <div class="form-group">
                             <div class="text-center">
-                                <img src="{{asset($testimonails->image)}}"
+                                <img src="{{asset($architectures->icon)}}"
                                      class="rounded-circle  h-25 w-25" alt="image slider">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label>Image</label>
+                            <label>Icon</label>
                             <label id="projectinput7" class="file center-block">
-                                <input type="file" id="file" name="image">
+                                <input type="file" id="file" name="icon">
                                 <span class="file-custom"></span>
                             </label>
-                            @error('image') <span class="text-danger">{{$message}}</span> @enderror
+                            @error('icon') <span class="text-danger">{{$message}}</span> @enderror
                         </div>
 
                         <div class="form-body">
                             <div class="form-row">
 
                                 <div class="form-group col-md-6">
-                                    <label>Name</label>
-                                    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ $testimonails->name }}" required>
-                                    @error('name') <div class="alert alert-danger">{{ $message }}</div> @enderror
-                                </div>
-
-                                <div class="form-group col-md-6">
                                     <label>Title </label>
-                                    <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" value="{{ $testimonails->title }}" required>
+                                    <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" value="{{ $architectures->title }}" required>
                                     @error('title') <div class="alert alert-danger">{{ $message }}</div> @enderror
                                 </div>
-
-                            </div>
-
-                            <div class="form-row">
+                                
                                 <div class="form-group col-md-6">
                                     <label>Description </label>
-                                    <input type="text" name="desc" class="form-control @error('desc') is-invalid @enderror" value="{{ $testimonails->desc }}" required>
+                                    <input type="text" name="desc" class="form-control @error('desc') is-invalid @enderror" value="{{ $architectures->desc }}" required>
                                     @error('desc') <div class="alert alert-danger">{{ $message }}</div> @enderror
                                 </div>
                             </div>
